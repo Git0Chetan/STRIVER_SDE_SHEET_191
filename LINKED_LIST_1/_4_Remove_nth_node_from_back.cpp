24da693377e9b2501d395bed2313a0874a493f24
chetan <chetan248581@gmail.com>
2023-07-12 09:56:57 +0530

    added Remove Nth Node from Back of a linked list

---
+++ LINKED_LIST_1/_4_Remove_nth_node_from_back.cpp
@@ -0,0 +1,45 @@
+// Remove N-th node from the end of a Linked List
+// Problem Statement: Given a linked list, and a number N. 
+// Find the Nth node from the end of this linked list and delete it. 
+// Return the head of the new modified linked list.
+
+
+//Leetcode:19   link:https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+
+
+#include<iostream>
+using namespace std;
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        ListNode* slow=head;
+        ListNode* fast=head;
+
+        while(n--){
+            fast=fast->next;
+        }
+
+        if(fast==NULL){
+            head=slow->next;
+            return head;
+        }
+
+        while(fast->next){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        slow->next=slow->next->next;
+        return head;
+    }
+};
+
