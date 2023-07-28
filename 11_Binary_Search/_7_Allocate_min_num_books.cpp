dc11f7282e59fde1ac6cf7b3051f03d00d1e6a83
chetan <chetan249581@gmail.com>
2023-07-28 16:43:33 +0530

    Allocate min num of pages problem added

---
+++ 11_Binary_Search/_7_Allocate_min_num_books.cpp
@@ -0,0 +1,65 @@
+// Allocate Minimum Number of Pages
+// Problem Statement: Given an array ‘arr of integer numbers, ‘ar[i]’ represents the number of pages in the ‘i-th’ book. There are a ‘m’ number of students, and the task is to allocate all the books to the students.
+// Allocate books in such a way that:
+// Each student gets at least one book.
+// Each book should be allocated to only one student.
+// Book allocation should be in a contiguous manner.
+// You have to allocate the book to ‘m’ students such that the maximum number of pages assigned to a student is minimum. If the allocation of books is not possible. return -1
+
+
+//gfg  link: https://practice.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1
+
+#include<iostream>
+using namespace std;
+
+bool isPossible(int arr[], int n, int m, int mid) {
+    int studentCount = 1;
+    int pageSum = 0;
+    
+    for(int i = 0; i<n; i++) {
+        if(pageSum + arr[i] <= mid) {
+            pageSum += arr[i];
+        }
+        else
+        {
+            studentCount++;
+            if(studentCount > m || arr[i] > mid ) {
+            return false;
+        }
+            pageSum = arr[i];
+        }
+        if(studentCount > m) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int findPages(int arr[], int n, int m) {
+    if(m>n){
+        return -1;
+    }
+    int s = 0;
+    int sum = 0;
+    
+    for(int i = 0; i<n; i++) {
+        sum += arr[i];
+    }
+    int e = sum;
+    int ans = -1;
+    int mid = s + (e-s)/2;
+    
+    while(s<=e)
+    {
+        if(isPossible(arr,n,m,mid)) {
+            ans = mid;
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
