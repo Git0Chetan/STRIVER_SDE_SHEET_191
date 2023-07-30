16a14603f5316c233238a556cb7503214f77cf0d
chetan <chetan249581@gmail.com>
2023-07-30 10:15:43 +0530

    find median of live data stream

---
+++ 12_heaps/_4_Find_MEdian_in_data_stream.cpp
@@ -0,0 +1,49 @@
+// The median is the middle value in an ordered integer list. If the size of the list is even, 
+//there is no middle value, and the median is the mean of the two middle values.
+// For example, for arr = [2,3,4], the median is 3.
+// For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+// Implement the MedianFinder class:
+// MedianFinder() initializes the MedianFinder object.
+// void addNum(int num) adds the integer num from the data stream to the data structure.
+// double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+
+//leetcode: 295  link: https://leetcode.com/problems/find-median-from-data-stream/description/
+
+
+#include<iostream>
+using namespace std;
+#include<queue>
+
+class MedianFinder {
+public:
+    priority_queue<int>maxi;
+    priority_queue<int,vector<int>,greater<int> >mini;
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        maxi.push(num);
+        mini.push(maxi.top());
+        maxi.pop();
+        if(mini.size()>maxi.size()){
+            maxi.push(mini.top());
+            mini.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(maxi.size()==mini.size()){
+            float ans= (maxi.top()+mini.top())/2.0;
+            return ans;
+        }
+        return maxi.top();
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
