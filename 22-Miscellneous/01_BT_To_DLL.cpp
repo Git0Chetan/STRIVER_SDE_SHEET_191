ee0e77904ba3bceb5c0a5a4b9e3b29fcc2d6965d
chetan <chetan249581@gmail.com>
2023-09-01 08:02:32 +0530

    Binary to Doubly Linked list is converted

---
+++ 22-Miscellneous/01_BT_To_DLL.cpp
@@ -0,0 +1,49 @@
+//Binary Tree to DLL
+//Given a Binary Tree (BT), convert it to a Doubly Linked List(DLL) In-Place. 
+//The left and right pointers in nodes are to be used as previous and next pointers respectively in converted DLL. 
+//The order of nodes in DLL must be same as Inorder of the given Binary Tree. 
+//The first node of Inorder traversal (leftmost node in BT) must be the head node of the DLL.
+//Note: H is the height of the tree and this space is used implicitly for the recursion stack.
+
+
+
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+class Solution
+{
+    public: 
+    
+    void bd(Node* root,Node* &prev){
+        if(root==NULL){
+            return;
+        }
+
+        bd(root->right,prev);
+        root->right=prev;
+        if(prev!=NULL)
+        prev->left=root;
+        prev=root;
+        bd(root->left,prev);
+    }
+    
+    Node * bToDLL(Node *root)
+    {
+        Node* prev = NULL;
+        bd(root,prev);
+        return prev;
+    }
+};
+
