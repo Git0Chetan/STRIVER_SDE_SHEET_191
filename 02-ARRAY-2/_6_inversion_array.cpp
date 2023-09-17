d31da85b0ccbd83c0ceb5fa907acce4992f7bd49
chetan <chetan249581@gmail.com>
2023-09-17 07:36:30 +0530

    Inversion array problem solved using merge sort

--- 02-ARRAY-2/_6_inversion_array.cpp
+++ 02-ARRAY-2/_6_inversion_array.cpp
@@ -35,3 +35,68 @@ int main()
 
 
 //not able to get the optimised solution:
+class Solution{
+  public:
+     long long count;
+    
+   
+void merge(long long v[],int l,int mid,int h)
+{
+    
+    long long i=l,j=mid+1;
+    vector<long long > vec;
+    
+    while(i<=mid && j<=h)
+    {
+        if(v[i]<=v[j])
+        {
+            vec.push_back(v[i]);
+            i++;
+        }
+        else{
+            count+=mid+1-i;
+            vec.push_back(v[j]);
+            j++;
+        }
+    }
+    
+    while(i<=mid)
+    {
+        vec.push_back(v[i]);
+        i++;
+    }
+    while(j<=h)
+    {
+        vec.push_back(v[j]);
+        j++;
+    }
+    
+    
+    for(int i=l;i<=h;i++)
+    {
+        v[i]=vec[i-l];
+    }
+}
+    
+    void mergeSort(long long arr[],int low,int high)
+    {
+        if(low<high)
+        {
+            long long mid=low+(high-low)/2;
+            mergeSort(arr,low,mid);
+            mergeSort(arr,mid+1,high);
+            merge(arr,low,mid,high);
+        }
+    }
+    
+    long long int inversionCount(long long arr[], long long N)
+    {
+        // Your Code Here
+       count=0;
+       
+        mergeSort(arr,0,N-1);
+    
+        return count;
+        
+    }
+};
